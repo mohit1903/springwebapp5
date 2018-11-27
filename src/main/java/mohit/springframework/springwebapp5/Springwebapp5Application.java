31604ad9dffb49ea9468c985755890dd07d96619
Mohit <mohit190392@gmail.com>
2018-11-27 23:13:34 +0530

    Adding hibernate

---
+++ src/main/java/mohit/springframework/springwebapp5/Springwebapp5Application.java
@@ -0,0 +1,12 @@
+package mohit.springframework.springwebapp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebapp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springwebapp5Application.class, args);
+	}
+}
